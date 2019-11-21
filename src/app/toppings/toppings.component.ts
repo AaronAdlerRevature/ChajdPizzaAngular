a0c0878a9d405ef2dd279eea20eab7f0c0ed450d
Cameron Marion <CSMarion@student.fullsail.edu>
2019-11-21 18:00:10 -0500

    Created Toppings component, for testing HTTP Get from local API. Web service needs updated for Enabling CORS.

---
+++ src/app/toppings/toppings.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+
+import { ToppingsService } from "../toppings.service";
+import { SizesService } from "../sizes.service";
+
+import { Topping } from '../topping';
+
+@Component({
+  selector: 'app-toppings',
+  templateUrl: './toppings.component.html',
+  styleUrls: ['./toppings.component.scss']
+})
+export class ToppingsComponent implements OnInit {
+
+  allToppings;
+  allSizes;
+
+  currentToppings:string[];
+  toppingCount:number = 0;
+  toppingPrice:number = 0;
+  
+  constToppingPrice:number = 1.5;
+
+  selectedSize:number = 1;
+  sizePrice:number = 0;
+
+  totalPrice:number = 0;
+
+  constructor(private topData:ToppingsService, private sizeData:SizesService) { }
+
+  ngOnInit() {
+    this.toppingCount=0;
+    this.currentToppings = [];
+
+    this.getToppings();
+    this.getSizes();  
+
+    //console.log(this.allSizes[0]);
+    //this.updateSize(1,0);
+  }
+
+  getToppings(){
+    this.topData.getToppings().subscribe(inData=>{ /*console.log(inData)*/; this.allToppings = inData; /*console.log(this.allToppings[0])*/});
+  }
+  
+  getSizes(){
+    this.sizeData.getSizes().subscribe(inData=>{ /*console.log(inData)*/; this.allSizes = inData; console.log(this.allSizes[0]); this.sizePrice=this.allSizes[this.selectedSize-1].s_Price; this.calculateNewPrice()});
+  }
+  
+  updateSize(size:number, i:number){
+    this.selectedSize = size;
+    this.sizePrice = this.allSizes[i].s_Price;
+
+    this.calculateNewPrice();
+  }
+  
+  updateTopping(element, top){
+    
+    //console.log(element);
+    //console.log(top);
+    if (element.target.checked){
+      let i:number = this.currentToppings.indexOf(top);
+      if (i === -1){
+        this.currentToppings.push(top);
+        ++this.toppingCount;
+        this.toppingPrice += this.constToppingPrice;
+      }
+    }else{
+      let i:number = this.currentToppings.indexOf(top);
+      if (i !== -1){
+        this.currentToppings.splice(i,1);
+        --this.toppingCount;
+        this.toppingPrice -= this.constToppingPrice;
+      }
+      
+    }
+    
+    console.log(this.currentToppings);
+    console.log(this.toppingCount);
+    this.calculateNewPrice();
+  }
+  
+  calculateNewPrice(){
+    this.totalPrice = this.toppingPrice + this.sizePrice;
+  }
+}
