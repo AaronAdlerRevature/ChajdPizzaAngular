54b54b75a7c4944074eecad2c7b195e97f56acc8
Cameron Marion <Cameron-Marion@revature.net>
2019-11-22 15:24:37 -0500

    Created data object class for Orders.

---
+++ src/app/data-classes/order.ts
@@ -0,0 +1,10 @@
+export class Order {
+         Id:number; 
+         CustomerId:number;
+         NetPrice:number;
+         isCompleted:boolean; 
+         TimePlaced:any;
+         DeliveryAddress:string;
+         Customer:any;
+         OrderDetails:any;
+}
