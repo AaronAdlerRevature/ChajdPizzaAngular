4137f1d3cd5d99f73b3a26ca2c41d5a8196e5dac
Cameron Marion <Cameron-Marion@revature.net>
2019-11-22 09:58:30 -0500

    Added HTTP Get methods for specialty-pizza.service.ts.

--- src/app/specialty-pizza.service.ts
+++ src/app/specialty-pizza.service.ts
@@ -9,4 +9,25 @@ import { SpecialtyPizza } from "./specialty-pizza";
 export class SpecialtyPizzaService {
 
   constructor(private client:HttpClient) { }
+
+  getSpecialPizzas(){
+    return this.client.get<SpecialtyPizza>('http://localhost:10531/api/PizzaTypesAPI/special');
+  }
+
+  getSpecialPizza(i:number){
+    return this.client.get<SpecialtyPizza>('http://localhost:10531/api/PizzaTypesAPI/special/' + i);
+  }
+
+  getSpecialPizzaName(i:number){
+    return this.client.get<string>('http://localhost:10531/api/PizzaTypesAPI/special/name/' + i);
+  }
+
+  getSpecialPizzaDescription(i:number){
+    return this.client.get<string>('http://localhost:10531/api/PizzaTypesAPI/special/desc/' + i);
+  }
+
+  getSpecialPizzaPrice(i:number){
+    return this.client.get<number>('http://localhost:10531/api/PizzaTypesAPI/special/price/' + i);
+  }
+
 }
