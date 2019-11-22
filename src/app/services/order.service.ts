d66da7c643323181e832e5679d1ad7db22740a93
Cameron Marion <Cameron-Marion@revature.net>
2019-11-22 15:40:38 -0500

    Created HTTP methods for order.service.ts.

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
+import { URL } from "../url";
+import { Order } from '../data-classes/order';
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
 
-  constructor() { }
+  constructor(private client:HttpClient) { }
+
+  getOrders(){
+    return this.client.get<Order>(URL.name + 'api/ordersapi');
+  }
+
+  getOrder(i:number){
+    return this.client.get<Order>(URL.name + 'api/ordersapi/' + i);
+  }
+
+  getOrderByCustomerID(i:number){
+    return this.client.get(URL.name + 'api/ordersapi/bycust/' + i);
+  }
+
+  checkMultipleOrdersOpen(i:number){
+    return this.client.get(URL.name + 'api/ordersapi/checkmultbycust/' + i, {responseType: 'text'});
+  }
+
+  updateOrder(o:Order){
+    return this.client.put(URL.name + 'api/ordersapi/' + o.Id, o);
+  }
+
+  deleteOrder(o:Order){
+    return this.client.delete(URL.name + 'api/ordersapi/' + o.Id);
+  }
 }
