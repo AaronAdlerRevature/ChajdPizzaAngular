1aae76cacaa3149f19fd6bd1618ae78f0bcf103b
Cameron Marion <Cameron-Marion@revature.net>
2019-11-22 16:42:33 -0500

    Created data object class for order details.

---
+++ src/app/data-classes/orderdetail.ts
@@ -0,0 +1,11 @@
+export class Orderdetail {
+        Id:number;
+        OrdersId:number;
+        SizeId:number;
+        ToppingsSelected:string;
+        ToppingsCount:number;
+        Price:number;
+        SpecialRequest:string;
+        Orders:any;
+        Size:any;
+}
