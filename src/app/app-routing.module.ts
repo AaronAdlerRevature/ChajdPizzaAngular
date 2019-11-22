2a59735c46f4a3c708b4d15c35186efd70bf47ab
Cameron Marion <Cameron-Marion@revature.net>
2019-11-22 11:51:52 -0500

    Added new testing component for getting customer information.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { MenuComponent } from "./menu/menu.component";
 import { ToppingsComponent  } from "./toppings/toppings.component";
+import { TestcustomerComponent } from './testcustomer/testcustomer.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'topping', component: ToppingsComponent },
+  { path: 'testcustomer', component: TestcustomerComponent},
 ];
 
 @NgModule({
--- src/app/testcustomer/testcustomer.component.ts
+++ src/app/testcustomer/testcustomer.component.ts
@@ -10,14 +10,15 @@ import { Customer } from '../data-classes/customer';
 export class TestcustomerComponent implements OnInit {
 
   currentCustomer:Customer;
-
+  customerID:number=0;
   constructor(private customerData:CustomerService) { }
 
   ngOnInit() {
+    this.customerID = 1;
   }
 
   getCustomer(i:number){
-    this.customerData.getCustomer(i).subscribe(inData=>{console.log(inData); });
+    this.customerData.getCustomer(i).subscribe(inData=>{console.log(inData); this.currentCustomer = inData;});
   }
 
 }
