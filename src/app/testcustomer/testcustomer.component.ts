1c4223ee827431f7288d8fdc34f07bcfa4e9c86c
Cameron Marion <Cameron-Marion@revature.net>
2019-11-22 13:24:16 -0500

    Created extra functionality for adding state information into customer information.

--- src/app/testcustomer/testcustomer.component.ts
+++ src/app/testcustomer/testcustomer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerService } from '../services/customer.service';
 import { Customer } from '../data-classes/customer';
+import { StatesService } from '../services/states.service';
 
 @Component({
   selector: 'app-testcustomer',
@@ -11,14 +12,23 @@ export class TestcustomerComponent implements OnInit {
 
   currentCustomer:Customer;
   customerID:number=0;
-  constructor(private customerData:CustomerService) { }
+  constructor(private customerData:CustomerService, private stateData:StatesService) { }
 
   ngOnInit() {
     this.customerID = 1;
   }
 
   getCustomer(i:number){
-    this.customerData.getCustomer(i).subscribe(inData=>{console.log(inData); this.currentCustomer = inData;});
+    this.customerData.getCustomer(i).subscribe(inData=>{
+                                              this.stateData.getStateName(inData.stateID).subscribe(secondData=>{console.log(secondData); inData.state = secondData});
+                                              console.log(inData); 
+                                              console.log(inData.city); 
+                                              console.log(inData.userName); 
+                                              console.log(inData.state); 
+                                              this.currentCustomer = inData;});
   }
 
+  getStateName(i:number){
+    this.stateData.getStateName(i).subscribe(inData=>{console.log(inData); this.currentCustomer.state = inData});
+  }
 }
