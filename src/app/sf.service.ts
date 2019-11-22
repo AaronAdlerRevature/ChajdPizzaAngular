ac63630ac8d0ec347ebf2209e0baeee0ff406ef6
Cameron Marion <Cameron-Marion@revature.net>
2019-11-22 10:20:34 -0500

    Created get functions for special formula service.

--- src/app/sf.service.ts
+++ src/app/sf.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { SF } from './sf';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SFService {
 
-  constructor() { }
+  constructor(private client:HttpClient) { }
+
+  getSFs(){
+    return this.client.get<SF>('http://localhost:10531/api/PizzaTypesAPI/sf');
+  }
+  
+  getSF(i:number){
+    return this.client.get<SF>('http://localhost:10531/api/PizzaTypesAPI/sf/' + i);
+  }
+
+  getSFPrice(i:number){
+    return this.client.get<number>('http://localhost:10531/api/PizzaTypesAPI/sf/price/' + i);
+  }
 }
