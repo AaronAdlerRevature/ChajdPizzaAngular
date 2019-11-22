7f60c212bc5d89cf8877938b7fcf7927d292549b
Cameron Marion <Cameron-Marion@revature.net>
2019-11-22 12:37:29 -0500

    Created functions in states.service.ts for getting state information.

--- src/app/services/states.service.ts
+++ src/app/services/states.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { State } from '../data-classes/state';
+import { URL } from '../url';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatesService {
 
-  constructor() { }
+  constructor(private client:HttpClient) { }
+
+  getStates(){
+    return this.client.get<State>(URL.name + 'api/stateapi');
+  }
+
+  getState(i:number){
+    return this.client.get<State>(URL.name + 'api/stateapi/' + i);
+  }
+
+  getStateName(i:number){
+    return this.client.get<State>(URL.name + 'api/stateapi/name/' + i);
+  }
+
+  getStateAbbreviation(i:number){
+    return this.client.get<State>(URL.name + 'api/stateapi/abbr/' + i);
+  }
 }
