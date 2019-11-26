e8247359904bd4ec142b2da75ad2e37c90800c76
Cameron Marion <Cameron-Marion@revature.net>
2019-11-26 11:41:32 -0500

    Created unit test for getting all customers.

--- src/app/services/customer.service.spec.ts
+++ src/app/services/customer.service.spec.ts
@@ -18,7 +18,7 @@ describe('CustomerService', () => {
         street: '123 A Street',
         city: 'Here',
         stateID: 1,
-        state:null,
+        state: null,
         zipCode: 12345,
       },
       {
@@ -28,17 +28,17 @@ describe('CustomerService', () => {
         street: '456 B Avenue',
         city: 'There',
         stateID: 2,
-        state:null,
+        state: null,
         zipCode: 98765,
       },
       {
-        id: 1,
+        id: 3,
         userName: 'Some@One.Com',
         name: 'SomeOne',
         street: '221 Baker Street',
         city: 'Somewhere',
         stateID: 3,
-        state:null,
+        state: null,
         zipCode: 31337,
       },
     ];
@@ -53,6 +53,49 @@ describe('CustomerService', () => {
       (mockHttp: HttpTestingController, testService: CustomerService) => {
         const service: CustomerService = TestBed.get(CustomerService);
         expect(service).toBeTruthy();
+      }));
+
+  it('should get all customers',
+    inject([HttpTestingController, CustomerService],
+      (mockHttp: HttpTestingController, testService: CustomerService) => {
+        // Mock call to HttpContext.
+        testService.getCustomerList().subscribe(inData => {
+          // Check response data.
+          expect(inData[0].id).toBe(1);
+          expect(inData[0].userName).toBe('John@Doe.Com');
+          expect(inData[0].name).toBe('John Doe');
+          expect(inData[0].street).toBe('123 A Street');
+          expect(inData[0].city).toBe('Here');
+          expect(inData[0].stateID).toBe(1);
+          expect(inData[0].state).toBeNull();
+          expect(inData[0].zipCode).toBe(12345);
+
+          expect(inData[1].id).toBe(2);
+          expect(inData[1].userName).toBe('Mary@Sue.Com');
+          expect(inData[1].name).toBe('Mary Sue');
+          expect(inData[1].street).toBe('456 B Avenue');
+          expect(inData[1].city).toBe('There');
+          expect(inData[1].stateID).toBe(2);
+          expect(inData[1].state).toBeNull();
+          expect(inData[1].zipCode).toBe(98765);
+
+          expect(inData[2].id).toBe(3);
+          expect(inData[2].userName).toBe('Some@One.Com');
+          expect(inData[2].name).toBe('SomeOne');
+          expect(inData[2].street).toBe('221 Baker Street');
+          expect(inData[2].city).toBe('Somewhere');
+          expect(inData[2].stateID).toBe(3);
+          expect(inData[2].state).toBeNull();
+          expect(inData[2].zipCode).toBe(31337);
+
+        });
+
+        // Check request data.
+        const request = mockHttp.expectOne(URL.name + 'api/customersapi');
+        expect(request.request.method).toEqual('GET');
+
+        // Fill request response.
+        request.flush(testData);
       }));
 
   afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
