ace44f9a17a5506dc49bf8eeef4b7d92991add80
Cameron Marion <Cameron-Marion@revature.net>
2019-11-25 20:03:43 -0500

    Created mock data for toppings.service.spec.ts.

--- src/app/services/toppings.service.spec.ts
+++ src/app/services/toppings.service.spec.ts
@@ -1,18 +1,40 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientTestingModule,  HttpTestingController } from '@angular/common/http/testing'
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 
 import { ToppingsService } from './toppings.service';
+import { Topping } from '../data-classes/topping';
 
 describe('ToppingsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [ToppingsService],
-    imports: [ HttpClientTestingModule ],
-  }));
 
-  it('should be created', inject([HttpTestingController, ToppingsService], (mockHttp: HttpTestingController, testService:ToppingsService) => {
-    const service: ToppingsService = TestBed.get(ToppingsService);
-    expect(service).toBeTruthy();
-  }));
+  let testData: Topping[];
+
+  beforeEach(() => {
+    testData = [
+      {
+        id: 1,
+        name: 'TopA',
+      },
+      {
+        id: 2,
+        name: 'TopB',
+      },
+      {
+        id: 3,
+        name: 'TopC',
+      },
+    ];
+    TestBed.configureTestingModule({
+      providers: [ToppingsService],
+      imports: [HttpClientTestingModule],
+    })
+  });
+
+  it('should be created',
+    inject([HttpTestingController, ToppingsService],
+      (mockHttp: HttpTestingController, testService: ToppingsService) => {
+        const service: ToppingsService = TestBed.get(ToppingsService);
+        expect(service).toBeTruthy();
+      }));
 
   afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
     httpMock.verify();
