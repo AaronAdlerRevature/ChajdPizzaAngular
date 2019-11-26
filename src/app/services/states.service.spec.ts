23efd3ca56679ce86b4e3d0d2c96b216c00d93bb
Cameron Marion <Cameron-Marion@revature.net>
2019-11-25 19:51:06 -0500

    Set mock data array definition to beforeEach in states.service.spec.ts.

--- src/app/services/states.service.spec.ts
+++ src/app/services/states.service.spec.ts
@@ -6,10 +6,32 @@ import { URL } from '../url';
 import { State } from '../data-classes/state';
 
 describe('StatesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [StatesService],
-    imports: [ HttpClientTestingModule ],
-  }));
+
+  let testData;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [StatesService],
+      imports: [ HttpClientTestingModule ],
+    });
+    testData = [
+      {
+        id : 1,
+        name : 'Florida',
+        abbreviation : 'FL',
+      },
+      {
+        id : 2,
+        name : 'Texas',
+        abbreviation : 'TX',
+      },
+      {
+        id : 3,
+        name : 'Virginia',
+        abbreviation : 'VA',
+      },
+    ];
+  });
 
   it('should be created', 
     inject([HttpTestingController, StatesService], 
@@ -22,42 +44,29 @@ describe('StatesService', () => {
   it('should get all states', 
   inject([HttpTestingController, StatesService], 
         (mockHttp: HttpTestingController, testService:StatesService) => {
-          const testData:State[] = [
-            {
-              id : 1,
-              name : 'Florida',
-              abbreviation : 'FL',
-            },
+              // Mock call to HttpContext.
+              testService.getStates().subscribe(inData=>{
+                    // Check response data.
+                    expect(inData[0].id).toBe(1);
+                    expect(inData[0].name).toBe('Florida');
+                    expect(inData[0].abbreviation).toBe('FL');
 
-            {
-              id : 2,
-              name : 'Texas',
-              abbreviation : 'TX',
-            },
-            
-            {
-              id : 3,
-              name : 'Virginia',
-              abbreviation : 'VA',
-            },
-            
-          ];
+                    expect(inData[1].id).toBe(2);
+                    expect(inData[1].name).toBe('Texas');
+                    expect(inData[1].abbreviation).toBe('TX');
 
-  testService.getStates().subscribe(inData=>{
-    expect(inData[0].id).toBe(1);
-    expect(inData[0].name).toBe('Florida');
-    expect(inData[0].abbreviation).toBe('FL');
+                    expect(inData[2].id).toBe(3);
+                    expect(inData[2].name).toBe('Virginia');
+                    expect(inData[2].abbreviation).toBe('VA');
+              });
 
-    expect(inData[1].id).toBe(2);
-    expect(inData[1].name).toBe('Texas');
-    expect(inData[1].abbreviation).toBe('TX');
-  });
-
-  const request = mockHttp.expectOne(URL.name + 'api/stateapi');
-  expect(request.request.method).toEqual('GET');
-
-  request.flush(testData);
-}));
+              // Check request data.
+              const request = mockHttp.expectOne(URL.name + 'api/stateapi');
+              expect(request.request.method).toEqual('GET');
+              
+              // Fill request response.
+              request.flush(testData);
+        }));
 
 
   afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
