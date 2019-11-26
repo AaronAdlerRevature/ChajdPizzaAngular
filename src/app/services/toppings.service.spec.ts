cf8d037b4e5e219f8a550ccaaab8db006866ad5d
Cameron Marion <Cameron-Marion@revature.net>
2019-11-25 19:15:27 -0500

    Created dependency injection of mock HttpClient, defined beforeEach and afterEach, for toppings.service.spec.ts.

--- src/app/services/toppings.service.spec.ts
+++ src/app/services/toppings.service.spec.ts
@@ -1,12 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule,  HttpTestingController } from '@angular/common/http/testing'
 
 import { ToppingsService } from './toppings.service';
 
 describe('ToppingsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [ToppingsService],
+    imports: [ HttpClientTestingModule ],
+  }));
 
-  it('should be created', () => {
+  it('should be created', inject([HttpTestingController, ToppingsService], (mockHttp: HttpTestingController, testService:ToppingsService) => {
     const service: ToppingsService = TestBed.get(ToppingsService);
     expect(service).toBeTruthy();
-  });
+  }));
+
+  afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    httpMock.verify();
+  }));
 });
