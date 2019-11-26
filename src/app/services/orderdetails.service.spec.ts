be760fa99ff634560fe888cf854cfd8b454c92af
Cameron Marion <Cameron-Marion@revature.net>
2019-11-26 08:46:16 -0500

    Created mock data for orderdetails.service.spec.ts.

--- src/app/services/orderdetails.service.spec.ts
+++ src/app/services/orderdetails.service.spec.ts
@@ -1,18 +1,63 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientTestingModule,  HttpTestingController } from '@angular/common/http/testing'
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 
 import { OrderdetailsService } from './orderdetails.service';
+import { URL } from '../url';
+import { Orderdetail } from '../data-classes/orderdetail';
 
 describe('OrderdetailsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [ OrderdetailsService ],
-    imports: [ HttpClientTestingModule ],
-  }));
 
-  it('should be created', inject([HttpTestingController, OrderdetailsService], (mockHttp: HttpTestingController, testService:OrderdetailsService) => {
-    const service: OrderdetailsService = TestBed.get(OrderdetailsService);
-    expect(service).toBeTruthy();
-  }));
+  let testData: Orderdetail[];
+
+  beforeEach(() => {
+    testData = [
+      {
+        id: 1,
+        ordersId: 1,
+        sizeId: 1,
+        toppingsSelected: 'A,B,C',
+        toppingsCount: 3,
+        price: 10.99,
+        specialRequest: 'None',
+        orders: null,
+        size: null,
+      },
+      {
+        id: 2,
+        ordersId: 1,
+        sizeId: 3,
+        toppingsSelected: 'C,D',
+        toppingsCount: 2,
+        price: 8.99,
+        specialRequest: 'None',
+        orders: null,
+        size: null,
+      },
+      {
+        id: 3,
+        ordersId: 2,
+        sizeId: 2,
+        toppingsSelected: 'A',
+        toppingsCount: 1,
+        price: 12.99,
+        specialRequest: 'NO',
+        orders: null,
+        size: null,
+      },
+    ];
+
+    TestBed.configureTestingModule({
+      providers: [OrderdetailsService],
+      imports: [HttpClientTestingModule],
+    })
+  });
+
+  it('should be created',
+    inject([HttpTestingController, OrderdetailsService],
+      (mockHttp: HttpTestingController, testService: OrderdetailsService) => {
+        const service: OrderdetailsService = TestBed.get(OrderdetailsService);
+        expect(service).toBeTruthy();
+      }));
 
   afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
     httpMock.verify();
