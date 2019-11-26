6c4c87b332c3dd51b52a4e8d4ca81506dadf5afe
Cameron Marion <Cameron-Marion@revature.net>
2019-11-26 08:51:55 -0500

    Created unit test for getting all order details.

--- src/app/services/orderdetails.service.spec.ts
+++ src/app/services/orderdetails.service.spec.ts
@@ -59,6 +59,52 @@ describe('OrderdetailsService', () => {
         expect(service).toBeTruthy();
       }));
 
+  it('should get all order details',
+    inject([HttpTestingController, OrderdetailsService],
+      (mockHttp: HttpTestingController, testService: OrderdetailsService) => {
+        // Mock call to HttpContext.
+        testService.getOrderDetails().subscribe(inData => {
+          // Check response data.
+          expect(inData[0].id).toBe(1);
+          expect(inData[0].ordersId).toBe(1);
+          expect(inData[0].sizeId).toBe(1);
+          expect(inData[0].toppingsSelected).toBe('A,B,C');
+          expect(inData[0].toppingsCount).toBe(3);
+          expect(inData[0].price).toBe(10.99);
+          expect(inData[0].specialRequest).toBe('None');
+          expect(inData[0].orders).toBeNull();
+          expect(inData[0].size).toBeNull();
+
+          expect(inData[1].id).toBe(2);
+          expect(inData[1].ordersId).toBe(1);
+          expect(inData[1].sizeId).toBe(3);
+          expect(inData[1].toppingsSelected).toBe('C,D');
+          expect(inData[1].toppingsCount).toBe(2);
+          expect(inData[1].price).toBe(8.99);
+          expect(inData[1].specialRequest).toBe('None');
+          expect(inData[1].orders).toBeNull();
+          expect(inData[1].size).toBeNull();
+
+          expect(inData[2].id).toBe(3);
+          expect(inData[2].ordersId).toBe(2);
+          expect(inData[2].sizeId).toBe(2);
+          expect(inData[2].toppingsSelected).toBe('A');
+          expect(inData[2].toppingsCount).toBe(1);
+          expect(inData[2].price).toBe(12.99);
+          expect(inData[2].specialRequest).toBe('NO');
+          expect(inData[2].orders).toBeNull();
+          expect(inData[2].size).toBeNull();
+
+        });
+
+        // Check request data.
+        const request = mockHttp.expectOne(URL.name + 'api/orderdetailsapi/');
+        expect(request.request.method).toEqual('GET');
+
+        // Fill request response.
+        request.flush(testData);
+      }));
+
   afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
     httpMock.verify();
   }));
