4d6fc603fc81945e0d5fc4f9c56f178a50a239e9
Cameron Marion <Cameron-Marion@revature.net>
2019-11-25 19:15:20 -0500

    Created dependency injection of mock HttpClient, defined beforeEach and afterEach, for states.service.spec.ts.

--- src/app/services/states.service.spec.ts
+++ src/app/services/states.service.spec.ts
@@ -1,12 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule,  HttpTestingController } from '@angular/common/http/testing'
 
 import { StatesService } from './states.service';
 
 describe('StatesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [StatesService],
+    imports: [ HttpClientTestingModule ],
+  }));
 
-  it('should be created', () => {
+  it('should be created', inject([HttpTestingController, StatesService], (mockHttp: HttpTestingController, testService:StatesService) => {
     const service: StatesService = TestBed.get(StatesService);
     expect(service).toBeTruthy();
-  });
+  }));
+
+  afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    httpMock.verify();
+  }));
 });
