87dc2ff062e70f2caafb561b37ae8eadb5f443d9
Cameron Marion <Cameron-Marion@revature.net>
2019-11-25 20:10:16 -0500

    Created mock data for specialty-pizza.service.spec.ts.

--- src/app/services/specialty-pizza.service.spec.ts
+++ src/app/services/specialty-pizza.service.spec.ts
@@ -1,18 +1,46 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientTestingModule,  HttpTestingController } from '@angular/common/http/testing'
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 
 import { SpecialtyPizzaService } from './specialty-pizza.service';
+import { SpecialtyPizza } from '../data-classes/specialty-pizza';
+import { URL } from '../url';
 
 describe('SpecialtyPizzaService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [ SpecialtyPizzaService ],
-    imports: [ HttpClientTestingModule ],
-  }));
+  let testData: SpecialtyPizza[];
 
-  it('should be created', inject([HttpTestingController, SpecialtyPizzaService], (mockHttp: HttpTestingController, testService:SpecialtyPizzaService) => {
-    const service: SpecialtyPizzaService = TestBed.get(SpecialtyPizzaService);
-    expect(service).toBeTruthy();
-  }));
+  beforeEach(() => {
+    testData = [
+      {
+        id: 1,
+        name: 'SpecialA',
+        price: 5.99,
+        description: 'StuffA',
+      },
+      {
+        id: 2,
+        name: 'SpecialB',
+        price: 7.99,
+        description: 'StuffB',
+      },
+      {
+        id: 3,
+        name: 'SpecialC',
+        price: 9.99,
+        description: 'StuffC',
+      },
+    ];
+    TestBed.configureTestingModule({
+      providers: [SpecialtyPizzaService],
+      imports: [HttpClientTestingModule],
+    })
+  });
+
+  it('should be created',
+    inject([HttpTestingController, SpecialtyPizzaService],
+      (mockHttp: HttpTestingController, testService: SpecialtyPizzaService) => {
+        const service: SpecialtyPizzaService = TestBed.get(SpecialtyPizzaService);
+        expect(service).toBeTruthy();
+      }));
 
   afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
     httpMock.verify();
