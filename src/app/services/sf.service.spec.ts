f92d18a2186bdba406c12b92040e6981705bc3ae
Cameron Marion <Cameron-Marion@revature.net>
2019-11-25 20:28:50 -0500

    Created mock data for sf.service.spec.ts.

--- src/app/services/sf.service.spec.ts
+++ src/app/services/sf.service.spec.ts
@@ -1,18 +1,37 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientTestingModule,  HttpTestingController } from '@angular/common/http/testing'
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 
 import { SFService } from './sf.service';
+import { URL } from '../url';
+import { SF } from '../data-classes/sf';
 
 describe('SFService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [ SFService ],
-    imports: [ HttpClientTestingModule ],
-  }));
 
-  it('should be created', inject([HttpTestingController, SFService], (mockHttp: HttpTestingController, testService:SFService) => {
-    const service: SFService = TestBed.get(SFService);
-    expect(service).toBeTruthy();
-  }));
+  let testData: SF[];
+
+  beforeEach(() => {
+    testData = [
+      {
+        id: 1,
+        price: 1.50,
+      },
+      {
+        id: 2,
+        price: 3.00,
+      },
+    ];
+    TestBed.configureTestingModule({
+      providers: [SFService],
+      imports: [HttpClientTestingModule],
+    })
+  });
+
+  it('should be created',
+    inject([HttpTestingController, SFService],
+      (mockHttp: HttpTestingController, testService: SFService) => {
+        const service: SFService = TestBed.get(SFService);
+        expect(service).toBeTruthy();
+      }));
 
   afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
     httpMock.verify();
