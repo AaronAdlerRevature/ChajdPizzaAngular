5419a0958a554a60ba87ab4cf3f197be959cf5cf
Cameron Marion <Cameron-Marion@revature.net>
2019-11-25 20:05:49 -0500

    Created unit test for getting all toppings.

--- src/app/services/toppings.service.spec.ts
+++ src/app/services/toppings.service.spec.ts
@@ -3,6 +3,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 
 import { ToppingsService } from './toppings.service';
 import { Topping } from '../data-classes/topping';
+import { URL } from "../url";
 
 describe('ToppingsService', () => {
 
@@ -34,6 +35,30 @@ describe('ToppingsService', () => {
       (mockHttp: HttpTestingController, testService: ToppingsService) => {
         const service: ToppingsService = TestBed.get(ToppingsService);
         expect(service).toBeTruthy();
+      }));
+
+  it('should get all toppings',
+    inject([HttpTestingController, ToppingsService],
+      (mockHttp: HttpTestingController, testService: ToppingsService) => {
+        // Mock call to HttpContext.
+        testService.getToppings().subscribe(inData => {
+          // Check response data.
+          expect(inData[0].id).toBe(1);
+          expect(inData[0].name).toBe('TopA');
+
+          expect(inData[1].id).toBe(2);
+          expect(inData[1].name).toBe('TopB');
+
+          expect(inData[2].id).toBe(3);
+          expect(inData[2].name).toBe('TopC');
+        });
+
+        // Check request data.
+        const request = mockHttp.expectOne(URL.name + 'api/PizzaTypesAPI/toppings');
+        expect(request.request.method).toEqual('GET');
+
+        // Fill request response.
+        request.flush(testData);
       }));
 
   afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
