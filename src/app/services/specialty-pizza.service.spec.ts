1cd73f51aaf24726acc8721123c00fa1b9bd3415
Cameron Marion <Cameron-Marion@revature.net>
2019-11-25 19:15:12 -0500

    Created dependency injection of mock HttpClient, defined beforeEach and afterEach, for specialty-pizza.service.spec.ts.

--- src/app/services/specialty-pizza.service.spec.ts
+++ src/app/services/specialty-pizza.service.spec.ts
@@ -1,12 +1,20 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
+import { HttpClientTestingModule,  HttpTestingController } from '@angular/common/http/testing'
 
 import { SpecialtyPizzaService } from './specialty-pizza.service';
 
 describe('SpecialtyPizzaService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [ SpecialtyPizzaService ],
+    imports: [ HttpClientTestingModule ],
+  }));
 
-  it('should be created', () => {
+  it('should be created', inject([HttpTestingController, SpecialtyPizzaService], (mockHttp: HttpTestingController, testService:SpecialtyPizzaService) => {
     const service: SpecialtyPizzaService = TestBed.get(SpecialtyPizzaService);
     expect(service).toBeTruthy();
-  });
+  }));
+
+  afterEach(inject([HttpTestingController], (httpMock: HttpTestingController) => {
+    httpMock.verify();
+  }));
 });
