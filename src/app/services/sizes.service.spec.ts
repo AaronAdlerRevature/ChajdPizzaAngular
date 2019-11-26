d129e9dcf9c0426801f7a9f054a9c433843e4e37
Cameron Marion <Cameron-Marion@revature.net>
2019-11-25 20:19:41 -0500

    Created mock data for sizes.service.spec.ts.

--- src/app/services/sizes.service.spec.ts
+++ src/app/services/sizes.service.spec.ts
@@ -2,12 +2,35 @@ import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientTestingModule,  HttpTestingController } from '@angular/common/http/testing'
 
 import { SizesService } from './sizes.service';
+import { URL } from '../url';
+import { Size } from '../data-classes/size';
 
 describe('SizesService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
+
+  let testData:Size[];
+
+  beforeEach(() =>{
+    testData = [
+      {
+        id: 1,
+        baseSize: 'Small', 
+        s_Price: 3.99,
+      },
+      {
+        id: 2,
+        baseSize: 'Medium',
+        s_Price: 4.99,
+      },
+      {
+        id: 3,
+        baseSize: 'Large',
+        s_Price: 5.99,
+      },
+    ];
+   TestBed.configureTestingModule({
     providers: [ SizesService ],
     imports: [ HttpClientTestingModule ],
-  }));
+  })});
 
   it('should be created', inject([HttpTestingController, SizesService], (mockHttp: HttpTestingController, testService:SizesService) => {
     const service: SizesService = TestBed.get(SizesService);
